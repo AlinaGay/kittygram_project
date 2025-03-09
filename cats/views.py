09e54812c31f1b80808e63ce463daec22c043f0e
AlinaGay <gayfullina.a@gmail.com>
2025-03-09 18:31:38 +0100

    Add view functions

--- cats/views.py
+++ cats/views.py
@@ -6,10 +6,15 @@ from .models import Cat
 from .serializers import CatSerializer
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'PATCH'])
 def cat_list(request):
     if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
+        cat = Cat.objects.get(id=id)
+        serializer = CatSerializer(
+            cat,
+            data=request.data,
+            many=True,
+            partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
